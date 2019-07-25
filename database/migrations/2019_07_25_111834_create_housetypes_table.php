5edeff269d40af178d4228c26557335f3fb3ff1d
Marshall <51704498+Marshall-Power@users.noreply.github.com>
2019-07-25 14:06:11 +0200

    Update 2019_07_25_111834_create_housetypes_table.php
    Udpated seeder HouseType to House_Type

--- database/migrations/2019_07_25_111834_create_housetypes_table.php
+++ database/migrations/2019_07_25_111834_create_housetypes_table.php
@@ -13,7 +13,7 @@ class CreateHousetypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('housetypes', function (Blueprint $table) {
+        Schema::create('house_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateHousetypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('housetypes');
+        Schema::dropIfExists('house_types');
     }
 }
