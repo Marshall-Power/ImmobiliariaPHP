f40389b4b80ddad6452842132c49080ae9d24381
Kevin Curtet <kcurtet@users.noreply.github.com>
2019-07-25 14:29:19 +0200

    feat: add HouseTypeSeeder to DatabaseSeeder
    He añadido el HouseTypeSeeder para que se ejecute en el db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTypeSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ProvinceTableSeeder::class);
+        $this->call(HouseTypeTableSeeder::class);
     }
 }
