ffbe817cc4e2ab91bc4fa742f1c52913e954cc87
Ruben <ruben.idigora@gmail.com>
2019-07-27 01:03:03 +0200

    View Home photos and cards added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
+  <div id="welcome" class="container">
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-4">
             @include('includes.filter')
                     </div>
-                    <div class="col-lg-8 col-md-8 col-sm-4">
+                <div class="col-lg-8 col-md-8 col-sm-4">
+                @forelse ($houses as $house)
+                    <div id="houses_row" class="row-fluid">
 
-                    <div>
-                        @forelse ($houses as $house)
-                            {{ $house->name }}
+                            <div class="col-lg-10 offset-lg-1">
+                                    <div class="row house_card">
+                                        <div class="col-lg-6 house_card">
+                                             <img src="{{ $house->photos->first()->path }}" alt="{{ $house->name }}">
+                                        </div>
+                                        <div class="col-lg-4 house_card">
+                                             <h2>{{ $house->name }}</h2>
+                                        </div>
+                                    </div>
+                            </div>
+                    </div>
                         @empty
 
                         @endforelse
-                    </div>
-                </div>
                 </div>
             </div>
+    </div>
 
 @endsection('content')
