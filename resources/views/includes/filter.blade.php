4759ea036f20d277b5735e01567cde7ed8b50f4f
Ruben <ruben.idigora@gmail.com>
2019-07-28 14:37:03 +0200

    updated filter include
    Created sliders for different parameters of houses

--- resources/views/includes/filter.blade.php
+++ resources/views/includes/filter.blade.php
@@ -20,22 +20,66 @@
     } );
 
     $( function() {
-      $( "#slider-range-price" ).slider({
+    $( "#slider-range-bathroom" ).slider({
+      range: true,
+      min: 1,
+      max: 3,
+      step: 1,
+      values: [ 1, 3 ],
+      slide: function( event, ui ) {
+        $( "#amount-bathroom" ).val(ui.values[ 0 ] + " baños - " + ui.values[ 1 ] + " baños " );
+      }
+    });
+    $( "#amount-bathroom" ).val( $( "#slider-range-bathroom" ).slider( "values", 0 ) + " baños  -  " +
+    $( "#slider-range-bathroom" ).slider( "values", 1 ) + " baños "  );
+  } );
+</script>
+<script>
+    $( function() {
+      $( "#slider-range-size" ).slider({
         range: true,
-        min: 100,
-        max: 5000,
-        step: 100,
-        values: [ 0, 5000 ],
+        min: 0,
+        max: 600,
+        step: 5,
+        values: [ 0, 600 ],
         slide: function( event, ui ) {
-          $( "#amount-price" ).val(ui.values[ 0 ] + " € - " + ui.values[ 1 ] + " € " );
+          $( "#amount-size" ).val(ui.values[ 0 ] + " m2 - " + ui.values[ 1 ] + " m2 " );
+          if ( ui.values[0] >= 100 ) $("#slider-range-size").slider('option', 'step', 10);
+          if ( ui.values[0] < 100 ) $("#slider-range-size").slider('option', 'step', 5);
         }
       });
-      $( "#amount-price" ).val( $( "#slider-range-price" ).slider( "values", 0 ) + " €  -  " +
-      $( "#slider-range-price" ).slider( "values", 1 ) + " € "  );
+      $( "#amount-size" ).val( $( "#slider-range-size" ).slider( "values", 0 ) + " m2  -  " +
+      $( "#slider-range-size" ).slider( "values", 1 ) + " m2 "  );
     } );
     </script>
+<script>
+
+    $( function() {
+        $( "#slider-range-price" ).slider({
+          range: true,
+          min: 100,
+          max: 5000,
+          step: 100,
+          values: [ 0, 5000 ],
+          slide: function( event, ui ) {
+            $( "#amount-price" ).val(ui.values[ 0 ] + " € - " + ui.values[ 1 ] + " € " );
+          }
+        });
+        $( "#amount-price" ).val( $( "#slider-range-price" ).slider( "values", 0 ) + " €  -  " +
+        $( "#slider-range-price" ).slider( "values", 1 ) + " € "  );
+      } );
+</script>
+<script>
+$( function() {
+    $( "#Rooms" ).selectmenu();
+  } );
+
+</script>
+
 
 @endpush
+
+
 
 
 
@@ -55,11 +99,40 @@
     <div id="slider-range-rooms" style="width:100%;"></div>
 </div>
 
+<div id="slider_bathroom_update_text" style="width:100%">
+        <p>
+            <label for="amount-bathroom">WC:</label>
+            <input type="text" id="amount-bathroom" readonly style="border:0; color:#f6931f; font-weight:bold;width:100%;">
+        </p>
+        <div id="slider-range-bathroom" style="width:100%;"></div>
+</div>
+
 <div id="slider_price_update_text" style="width:100%">
     <p>
-        <label for="amount">Rango de precios:</label>
+        <label for="amount-price">Rango de precios:</label>
         <input type="text" id="amount-price" readonly style="border:0; color:#f6931f; font-weight:bold;width:100%;">
     </p>
     <div id="slider-range-price" style="width:100%;"></div>
 </div>
+
+
+<div id="slider_size_update_text" style="width:100%">
+        <p>
+            <label for="amount-size">Metros cuadrados:</label>
+            <input type="text" id="amount-size" readonly style="border:0; color:#f6931f; font-weight:bold;width:100%;">
+        </p>
+        <div id="slider-range-size" style="width:100%;"></div>
+</div>
+
+<fieldset>
+<label for="Rooms">Select a speed</label>
+<select name="Rooms" id="Rooms">
+  <option selected="selected">1</option>
+  <option>2</option>
+  <option>3</option>
+  <option>4</option>
+  <option>5</option>
+</select>
+
+
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
             @include('includes.filter')
         </div>
         <div class="col-lg-9 col-md-9 col-sm-4 houses_div">
-            <div class="btn btn-success offset-lg-1">
+            <div class="btn btn-primary offset-lg-1">
                 <p>Comprar</p>
             </div>
-            <div class="btn btn-success">
+            <div class="btn btn-primary">
                 <p>Alquilar</p>
             </div>
             <hr class="offset-lg-1">
