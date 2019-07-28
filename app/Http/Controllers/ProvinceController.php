b44e2a58c425154e761b311122bedddfbc584d56
Kevin Curtet <kcurtet@users.noreply.github.com>
2019-07-28 13:25:45 +0200

    feat: add ProcinceController

---
+++ app/Http/Controllers/ProvinceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Province;
+use Illuminate\Http\Request;
+
+class ProvinceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.provinces.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.provinces.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        redirect()->route('provinces.index')
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Province $province)
+    {
+        return view('admin.provinces.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Province $province)
+    {
+        return view('admin.provinces.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Province $province)
+    {
+        return redirect()->route('provinces.show', $province)
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Province  $province
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Province $province)
+    {
+        return redirect()->route('provinces.index');
+    }
+}
