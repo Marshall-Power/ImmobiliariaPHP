6c70ce4f3d8dd0ad69fe13bfde764a0863c8cbc8
Marshall <Marshall-Power@users.noreply.github.com>
2019-07-29 13:54:32 +0200

    Mapa Api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.app')
 @include('cookieConsent::index')
-
+@section('css')
+<style>
+  /* Set the size of the div element that contains the map */
+  #map {
+    height: 400px;  /* The height is 400 pixels */
+    width: 100%;  /* The width is the width of the web page */
+    }
+</style>
+@endsection
 @section('content')
 <div id="welcome" class="container">
     <div class="row">
@@ -15,10 +23,79 @@
                 Alquilar
             </div>
             <hr class="offset-lg-1">
+            <h3>Habitatges a Girona</h3>
+   
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+      // Initialize and add the map
+      /*function initMap() {
+        // The location of Girona
+        var girona = {lat: 41.983333, lng: 2.816667};
+        // The map, centered at Girona
+        var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: girona});
+        
 
+        // Example marker, positioned at Girona
+        var mansio1 = {lat: 41.9963105, lng: 2.8268954};
+        var contentString = '';
+        
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        var marker1 = new google.maps.Marker({position: mansio1, 
+                                              map: map, 
+                                              title: "Mansió 1, Girona"});
+        marker1.addListener('click', function() {
+          infowindow.open(map, marker1);
+        });
+      };*/
+ 
+      function initMap(){      
+      $.ajax({
+        url:'{{url('/api/houses')}}',
+        method:"POST",
+        dataType:"JSON",
+        success:function(result){
+          // The location of Girona
+          var girona = {lat: 41.983333, lng: 2.816667};
+          // The map, centered at Girona
+          var map = new google.maps.Map(document.getElementById('map'), {zoom: 12, center: girona});
+          
+          result.forEach(function(item, index){
+            var casa = {lat: parseFloat(item.latitude), lng: parseFloat(item.longitude)};
+            
+            var contentString = "<div class='card' style='width: 18rem;'>
+              <img class='card-img-top' src='...' alt='Card image cap'>
+              <div class='card-body'>
+                <h5 class='card-title'>Card title</h5>
+                <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href='#' class='btn btn-primary'>Go somewhere</a>
+              </div>
+            </div>";
+            
+            var infowindow = new google.maps.InfoWindow({
+              content: contentString
+            });
+            var marker = new google.maps.Marker({position: casa, 
+                                                  map: map, 
+                                                  title: item.name});
+            marker.addListener('click', function() {
+              infowindow.open(map, marker);
+            });
+            marker.setMap(map);
+          });
+        console.log();         
+        }
+      });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC_usw0PXe09QidUHvTnTYhQJWCIaj64CU&callback=initMap">
+    </script>
+    
             <div class="row houses_row">
                 @forelse ($houses as $house)
-
 
                 <div class="col-lg-12 offset-lg-1 house_card_main">
                     <div class="row house_card">
