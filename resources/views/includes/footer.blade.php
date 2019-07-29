60e77eeb52349156da48bcb9711143fcb56a056d
Ruben <ruben.idigora@gmail.com>
2019-07-29 16:47:37 +0200

    Pagination and card recreated

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,6 @@
-<div id="footer" class="container-fluid">
+
+<div id="footer" class="">
+    <div class="container">
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
             <h3>Mas acerca de immo pro</h3>
@@ -36,4 +38,5 @@
         <div>
             <p>Copyrighted</p>
         </div>
+</div>
 </div>
