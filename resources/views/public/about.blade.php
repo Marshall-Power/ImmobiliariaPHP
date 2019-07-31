c2715898c5a1adbbb5d061b8abf25f4f631d4b42
Marshall <51704498+Marshall-Power@users.noreply.github.com>
2019-07-31 10:50:24 +0200

    updated about
    en realidad apenas he tocado nada lo que parece mucho por alguna razon xD

--- resources/views/public/about.blade.php
+++ resources/views/public/about.blade.php
@@ -2,25 +2,22 @@
 @section('content')
 
 <div class="container">
-<div class="row">
-<div class="col-lg-4 col-md-4 col-sm-4">
-<h1>{{trans('messages.aboutUs')}}</h1>
-</div>
-</div>
-<div class="row">
-<div class="col-lg-4 col-md-4 col-sm-12">
-<img src="http://www.refincel.com/images/sobre-nosotros-icon-max.png">
-</div>
-<div class="col-lg-8 col-md-8 col-sm-12">
-<p>Lorem ipsum dolor sit amet consectetur adipiscing elit nunc ultrices, bibendum blandit consequat ullamcorper nam dictum placerat purus, posuere inceptos dictumst feugiat vivamus elementum non ante. Mi urna ad faucibus sodales ornare sociis mauris, tellus integer hendrerit pretium felis at, semper dui porta magna id gravida. A non etiam iaculis posuere interdum donec eleifend magna varius tincidunt, blandit molestie class nisi nullam enim sodales vulputate.
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4">
+      <h1>{{trans('messages.aboutUs')}}</h1> 
+    </div>
+    
+  </div>
+  <div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-12">
+      <img src="http://www.refincel.com/images/sobre-nosotros-icon-max.png">
+    </div>
+    <div class="col-lg-8 col-md-8 col-sm-12">
+        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit nunc ultrices, bibendum blandit consequat ullamcorper nam dictum placerat purus, posuere inceptos dictumst feugiat vivamus elementum non ante. Mi urna ad faucibus sodales ornare sociis mauris, tellus integer hendrerit pretium felis at, semper dui porta magna id gravida. A non etiam iaculis posuere interdum donec eleifend magna varius tincidunt, blandit molestie class nisi nullam enim sodales vulputate.
 
-Potenti fringilla eget mattis purus aliquam iaculis accumsan ultricies litora feugiat posuere magna ornare, nullam nostra dis sapien curae nisl quam ac ad nisi urna. Dui metus primis euismod habitant cubilia curabitur eu dapibus lectus, laoreet quam turpis duis proin ad convallis eleifend, lacinia tincidunt tempus et cras ut pellentesque lobortis. Nisi ante mus odio lacus congue sociosqu netus eu ornare, dictum convallis per nullam luctus ultricies diam tempus, erat litora quam fringilla vulputate consequat elementum pharetra.</p>
-</div>
-  <a class="btn btn-secondary" href="{{ url('/') }}">{{trans('messages.back')}}</a>
-
-</div>
-
-
-
+        Potenti fringilla eget mattis purus aliquam iaculis accumsan ultricies litora feugiat posuere magna ornare, nullam nostra dis sapien curae nisl quam ac ad nisi urna. Dui metus primis euismod habitant cubilia curabitur eu dapibus lectus, laoreet quam turpis duis proin ad convallis eleifend, lacinia tincidunt tempus et cras ut pellentesque lobortis. Nisi ante mus odio lacus congue sociosqu netus eu ornare, dictum convallis per nullam luctus ultricies diam tempus, erat litora quam fringilla vulputate consequat elementum pharetra.</p>
+    </div>
+  </div>
+  
 </div>
 @endsection
